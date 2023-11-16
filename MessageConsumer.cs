54e3fb2d5ca3fa2cb16d74f91db44c05e2d45a1c
Laura Bustamante <lbustamante@unal.edu.co>
2023-11-16 10:00:28 -0500

    Add message consumer implementation

---
+++ MessageConsumer.cs
@@ -0,0 +1,42 @@
+﻿// https://github.com/Kamil-Zakiev/kafka-samples/blob/master/KafkaSamples/TempAnalyzer/MessageConsumer.cs
+namespace KafkaMessageConsumer
+{
+    using Confluent.Kafka;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public class MessageConsumer : BackgroundService
+    {
+        private readonly IConsumer<string, string> _consumer;
+        private readonly ILogger<MessageConsumer> _logger;
+
+        public MessageConsumer(IConsumer<string, string> consumer, ILogger<MessageConsumer> logger)
+        {
+            _consumer = consumer;
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Yield();
+            var i = 0;
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var consumeResult = _consumer.Consume(stoppingToken);
+
+                _logger.LogInformation("Consumer: " + _consumer.Name + " - Event key: " + consumeResult.Message.Key + " - Event Value: " + consumeResult.Message.Value);
+
+                if (i++ % 1000 == 0)
+                {
+                    _consumer.Commit();
+                }
+            }
+        }
+
+        public override void Dispose()
+        {
+            _consumer.Dispose();
+            base.Dispose();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using KafkaMessageConsumer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var consumerConfig = new ConsumerConfig();
+        context.Configuration.GetSection("ConsumerConfig").Bind(consumerConfig);
+
+        var consumer1 = new ConsumerBuilder<string, string>(consumerConfig).Build();
+        consumer1.Subscribe("Discoteque.Albums");
+
+        var consumer2 = new ConsumerBuilder<string, string>(consumerConfig).Build();
+        consumer2.Subscribe("Discoteque.Albums");
+
+        services.AddHostedService(sp =>
+            new MessageConsumer(consumer1, sp.GetRequiredService<ILogger<MessageConsumer>>()));
+        services.AddHostedService(sp =>
+            new MessageConsumer(consumer2, sp.GetRequiredService<ILogger<MessageConsumer>>()));
+    })
+    .Build()
+    .Run();
